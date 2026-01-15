ed89f4b6c5fa5ccc29efd5a1fcb1b5d758b955a2
Suraj x Whp Team <suraj2830009@gmail.com>
2026-01-15 13:09:17 +0530

    CUPP.py

--- CUPP.py
+++ CUPP.py
@@ -164,8 +164,8 @@ def version():
     """Display version"""
 
     print("\r\n	\033[1;31m[ cupp.py ]  " + __version__ + "\033[1;m\r\n")
-    print("	* Hacked up by j0rgan - j0rgan@remote-exploit.org")
-    print("	* http://www.remote-exploit.org\r\n")
+    print("	* Hacked up by Suraj X Whp")
+    print("	* http://github.com/suraj2830009-arch\r\n")
     print("	Take a look ./README.md file for more info about the program\r\n")
 
 
